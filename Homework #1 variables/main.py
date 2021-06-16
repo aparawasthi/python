2c1f480f4ae94a9907e8d9d53cd5e1dc84bb7dc9
Apar Awasthi <aparawasthi1@gmail.com>
2021-06-16 09:29:41 +0530

    Python basic variables

---
+++ Homework #1 variables/main.py
@@ -0,0 +1,71 @@
+"""
+A Python script to create attributes of a song.
+Then printing those variable on the console.
+"""
+# Title of the song
+Title = "Numb"
+
+# Artist of the song
+Artist = "Linkin Park"
+
+# Album of the song
+Album = "Meteora"
+
+# Year of release of this song
+Year = 2003
+
+# Duration of the song
+Duration = "03:05"
+
+# Genre of the song
+Genre = "Alternative rock, Nu metal, Alt Metal"
+
+# lyrics for the song
+Lyrics = """I'm tired of being what you want me to be
+Feeling so faithless, lost under the surface
+I don't know what you're expecting of me
+Put under the pressure of walking in your shoes
+Caught in the undertow, just caught in the undertow
+Every step that I take is another mistake to you
+Caught in the undertow, just caught in the undertow
+
+I've become so numb, I can't feel you there
+Become so tired, so much more aware
+By becoming this all I want to do
+Is be more like me and be less like you
+
+Can't you see that you're smothering me?
+Holding too tightly, afraid to lose control
+'Cause everything that you thought I would be
+Has fallen apart right in front of you
+Caught in the undertow, just caught in the undertow
+Every step that I take is another mistake to you
+Caught in the undertow, just caught in the undertow
+And every second I waste is more than I can take!
+
+I've become so numb, I can't feel you there
+Become so tired, so much more aware
+By becoming this all I want to do
+Is be more like me and be less like you
+
+And I know I may end up failing too
+But I know you were just like me with someone disappointed in you
+
+I've become so numb, I can't feel you there
+Become so tired, so much more aware
+By becoming this all I want to do
+Is be more like me and be less like you
+
+I've become so numb, I can't feel you there
+I'm tired of being what you want me to be
+I've become so numb, I can't feel you there
+I'm tired of being what you want me to be"""
+
+# Displaying attributes of the song
+print("Title :", Title)
+print("Artist :", Artist)
+print("Album :", Album)
+print("Genre :", Genre)
+print("Year of Release :", Year)
+print("Duration :", Duration)
+print("Lyrics :", Lyrics)
